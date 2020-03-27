900da848bdc7d4e1378f3634745123734ffd8fcd
havveFn <46079709+havveFn@users.noreply.github.com>
2020-03-28 00:04:24 +0900

    feat: create bits class

---
+++ src/__test__/index.spec.ts
@@ -0,0 +1,6 @@
+describe('hoge', () => {
+  it('hogeeee', () => {
+    expect(1).toBe(1)
+    console.log('yay')
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,101 @@
-console.log('hello')
+const isNumber = (val: any): val is number => typeof val === 'number'
+const isString = (val: any): val is string => typeof val === 'string'
+const toBinaryString = (num: number) => num.toString(2)
+const toOneZeroChar = (val: boolean) => (val ? '1' : '0')
+
+export class Bits {
+  private arr: boolean[]
+
+  constructor(size: number, val: number | string) {
+    const binStr = (isNumber(val) && toBinaryString(val)) || (isString(val) && val) || ''
+
+    const binArr = binStr
+      .split('')
+      .reverse()
+      .map(v => v === '1')
+
+    this.arr = [...Array(size)].map((_, i) => (i < binArr.length ? binArr[i] : false))
+  }
+
+  at(pos: number) {
+    const { arr } = this
+
+    const get = () => {
+      if (pos < arr.length) {
+        return arr[pos]
+      } else {
+        return false
+      }
+    }
+
+    const set = (bit: boolean) => {
+      this.arr[pos] = bit
+    }
+
+    return (bit?: boolean) => {
+      if(bit === undefined) {
+        return get()
+      } else {
+        return set(bit)
+      }
+    }
+  }
+
+  get(pos: number) {
+    const { arr } = this
+
+    if (pos < arr.length) {
+      return arr[pos]
+    } else {
+      return false
+    }
+  }
+
+  set(pos: number) {
+    const { arr } = this
+
+    if (pos < arr.length) {
+      return (bit: boolean) => {
+        this.arr[pos] = bit
+      }
+    }
+  }
+
+  flip() {
+    const { arr } = this
+
+    this.arr = [...arr].map(v => !v)
+  }
+
+  count() {
+    const { arr } = this
+
+    return arr.reduce((acc, curr) => {
+      return curr ? acc + 1 : acc
+    }, 0)
+  }
+
+  toString() {
+    const { arr } = this
+
+    return [...arr]
+      .reverse()
+      .map(toOneZeroChar)
+      .join('')
+  }
+
+  toNumber() {
+    const { arr } = this
+
+    const str = [...arr]
+      .reverse()
+      .map(toOneZeroChar)
+      .join('')
+
+    return parseInt(str, 2)
+  }
+}
+
+const bits = new Bits(4, '1110')
+
+console.log(bits.toString())
